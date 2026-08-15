91fac28ef443a91d3883c979ba13be564e300b93
Shivaji <shivaji.chakravorty5@gmail.com>
2026-08-16 00:01:55 +0530

    Deployment initiated

--- dj_ai_employee_main/settings.py
+++ dj_ai_employee_main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+
 
 
 # Application definition
@@ -47,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/orders/'
 LOGOUT_REDIRECT_URL = '/login'
@@ -132,4 +137,6 @@ LOGIN_URL = '/login'
 
 ANTHROPIC_API_KEY = config('ANTHROPIC_API_KEY')
 ANTHROPIC_MODEL = config('ANTHROPIC_MODEL')
+
+STATICFILES_STORAGE = 'whitenoise.noise.StaticFilesStorage'
 
